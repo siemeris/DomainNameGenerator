7ab79fff5b52d0f7f9ebf9fb55df42b986530d32
siemeris <siemeris@gmail.com>
2022-06-08 18:52:32 +0000

    domain hacks

--- app.js
+++ app.js
@@ -6,16 +6,16 @@
 //};
 
 
-  var pron = ["la", "nuestro"];
-  var adj = ["great", "big"];
-  var noun = ["joggercom", "jamones", "caracoles"];
+  var pron = ["los", "nuestros"];
+  var adj = ["geniales", "grandes"];
+  var noun = ["telecom", "caracoles", "desafio"];
   var ext = [".es", ".com", ".net", ".us", ".io"];
 
   for(var item of pron){
       for(var item1 of adj){
           for (var item2 of noun){
                 for (var item3 of ext){
-                    if (item2.includes(item3.slice(1,item3.length))){
+                    if (item2.slice(item2.length-item3.length,item2.length).includes(item3.slice(1,item3.length))){
                         //console.log(item2.indexOf(item3.slice(1,item3.length)));
                         console.log(item + item1 + item2.slice(0,item2.length+1-item3.length) + item3);
                     }
