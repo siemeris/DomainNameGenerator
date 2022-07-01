4ad8fa6e5e1bd098dc1c2f7d75806aeca1d0ab1d
siemeris <siemeris@gmail.com>
2022-07-01 15:11:06 +0000

    changing var names

--- app.js
+++ app.js
@@ -11,16 +11,16 @@
   const noun = ["telecom", "caracoles", "desafio", "internet", "repelus"];
   const ext = [".es", ".com", ".net", ".us", ".io"];
 
-  for(var item of pron){
-      for(var item1 of adj){
-          for (var item2 of noun){
-                for (var item3 of ext){
-                    if (item2.substring(item2.length-item3.length).includes(item3.substring(1))){
+  for(var pronom of pron){
+      for(var adjetivo of adj){
+          for (var nombre of noun){
+                for (var extension of ext){
+                    if (nombre.substring(nombre.length-extension.length).includes(extension.substring(1))){
                         //console.log(item2.indexOf(item3.slice(1,item3.length)));
-                        console.log(item + item1 + item2.slice(0,item2.length+1-item3.length) + item3);
+                        console.log(pronom + adjetivo + nombre.slice(0,nombre.length+1-extension.length) + extension);
                     }
                     else{
-                    console.log(item + item1 + item2 + item3);
+                    console.log(pronom + adjetivo + nombre + extension);
                 }
                 }
           }
